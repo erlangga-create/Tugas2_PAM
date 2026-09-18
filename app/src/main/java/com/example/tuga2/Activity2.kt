56ebdc15059b2b2b6d8efd1b4c6baafd81bdb8e9
vtronz <erlanggatri2005@gmail.com>
2026-09-18 16:40:11 +0700

    test 1

---
+++ app/src/main/java/com/example/tuga2/Activity2.kt
@@ -0,0 +1,58 @@
+package com.example.tuga2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import com.example.tuga2.ui.theme.Tuga2Theme
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class SecondActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        // Mengambil data yang dikirim dari MainActivity
+        val receivedText = intent.getStringExtra("text_input") ?: ""
+
+        setContent {
+            Tuga2Theme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(24.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Text(
+                        text = "NIM: 245150400111023"
+                    )
+
+                    Spacer(
+                        modifier = Modifier.height(8.dp)
+                    )
+
+                    Text(
+                        text = "Nama: Erlangga"
+                    )
+
+                    Spacer(
+                        modifier = Modifier.height(24.dp)
+                    )
+
+                    Text(
+                        text = receivedText
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tuga2/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.tuga2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tuga2.ui.theme.Tuga2Theme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tuga2Theme {
+                MainScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun MainScreen() {
+        val textInput = remember {
+            mutableStateOf("")
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "NIM: 245150400111023"
+            )
+
+            Spacer(
+                modifier = Modifier.height(8.dp)
+            )
+
+            Text(
+                text = "Nama: Erlangga"
+            )
+
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+
+            OutlinedTextField(
+                value = textInput.value,
+                onValueChange = { newText ->
+                    textInput.value = newText
+                },
+                label = {
+                    Text("Masukkan teks")
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+
+            Button(
+                onClick = {
+
+                    val intent = Intent(
+                        this@MainActivity,
+                        SecondActivity::class.java
+                    )
+
+                    intent.putExtra(
+                        "text_input",
+                        textInput.value
+                    )
+
+                    startActivity(intent)
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Kirim")
+            }
+        }
+    }
+}
